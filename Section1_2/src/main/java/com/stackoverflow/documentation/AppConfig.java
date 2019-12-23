7fcddf1067e50fe287950c87f559d6752e7bbce9
EugeneZakhno <zohan1234567@gmail.com>
2019-12-23 22:09:29 +0300

    added Section1_3

--- Section1_2/src/main/java/com/stackoverflow/documentation/AppConfig.java
+++ Section1_2/src/main/java/com/stackoverflow/documentation/AppConfig.java
@@ -3,6 +3,7 @@ package com.stackoverflow.documentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @ComponentScan("com.stackoverflow.documentation")
 public class AppConfig {
