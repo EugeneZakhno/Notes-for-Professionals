39923c90e3c3faf0e3fc601ffd090f9f9479eabf
EugeneZakhno <zohan1234567@gmail.com>
2019-12-29 00:03:27 +0300

    finished Section2_2

---
+++ Section2_2/src/test/java/FlipKartTest.java
@@ -0,0 +1,13 @@
+//Test class
+import com.sdp.component.Courier;
+import com.sdp.service.FlipKart;
+import com.sdp.util.ObjectFactory;
+public class FlipKartTest {
+    public static void main(String[] args) {
+        Courier courier=(Courier)ObjectFactory.getInstance("Dtdc.class");
+        FlipKart flipkart=(FlipKart)ObjectFactory.getInstance("FlipKart.class");
+        flipkart.setCourier(courier);
+        flipkart.shopping("Hp Laptop", "SR Nagar,Hyderabad");
+
+    }
+}
