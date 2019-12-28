b6c1cf66f6cc0f2bfd2ff22b476263bd7cc42998
EugeneZakhno <zohan1234567@gmail.com>
2019-12-28 23:45:21 +0300

    added Section2_2

---
+++ Section2_2/src/main/java/com/sdp/component/BlueDart.java
@@ -0,0 +1,11 @@
+
+//implementation classes
+  package com.sdp.component;
+
+public class BlueDart implements Courier {
+
+    public String deliver(String iteams, String address) {
+
+        return iteams+ "Shiped to Address "+address +"Through BlueDart";
+    }
+}
---
+++ Section2_2/src/main/java/com/sdp/component/Courier.java
@@ -0,0 +1,5 @@
+//Interface
+package com.sdp.component;
+public interface Courier {
+    public String deliver(String iteams,String address);
+}
